56d4d7b26ede6fed003b2ee92a2f58333c558d7e
Fede Diaz <nordri@gmail.com>
2018-04-05 15:00:41 +0200

    lets do some debugging

--- kurento_ci_container_job_setup.sh
+++ kurento_ci_container_job_setup.sh
@@ -266,8 +266,8 @@ docker run \
   -u "root" \
   -w "$CONTAINER_WORKSPACE" \
     $CONTAINER_IMAGE \
+    sleep 300
       #$KURENTO_SCRIPTS_HOME/kurento_ci_container_entrypoint.sh $BUILD_COMMAND
-      sleep 300
 status=$?
 
 # Change worspace ownership to avoid permission errors caused by docker usage of root
