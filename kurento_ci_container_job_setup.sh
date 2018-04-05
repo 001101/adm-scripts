c7655fbbdcf8ee472e74fb8bed32c3accd804c1b
Fede Diaz <nordri@gmail.com>
2018-04-05 12:59:42 +0200

    what can you see the custom tools?

--- kurento_ci_container_job_setup.sh
+++ kurento_ci_container_job_setup.sh
@@ -133,7 +133,7 @@ docker run \
   -v /var/jenkins_home/test-files:$CONTAINER_TEST_FILES \
   -w $CONTAINER_TEST_FILES \
   kurento/svn-client:1.0.0 \
-  find /
+  echo $KURENTO_SCRIPTS_HOME
 #  $CONTAINER_ADM_SCRIPTS/kurento_update_test_files.sh || {
 #    echo "[kurento_ci_container_job_setup] ERROR: Command failed: docker run kurento_update_test_files"
 #    exit $?
