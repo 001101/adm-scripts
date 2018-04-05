0194f0ffd36b0142ee6012fe41896e3b50b9bae3
Fede Diaz <nordri@gmail.com>
2018-04-05 14:41:30 +0200

    adjunsting the script

--- kurento_ci_container_entrypoint.sh
+++ kurento_ci_container_entrypoint.sh
@@ -39,12 +39,14 @@ if [ -n "$UBUNTU_PRIV_S3_ACCESS_KEY_ID" ] && [ -n "$UBUNTU_PRIV_S3_SECRET_ACCESS
   Token = ''" >/etc/apt/s3auth.conf
 fi
 
-apt-get install -y wget iproute
+apt-get update && \
+  apt-get install -y wget iproute
 wget http://archive.ubuntu.com/ubuntu/pool/main/libt/libtimedate-perl/libtimedate-perl_2.3000-2_all.deb
 dpkg -i libtimedate*deb
 rm libtimedate*deb
 
-wget -O - http://ubuntu.kurento.org/kurento.gpg.key | apt-key add -
+# Installing Kurento Media Server PGP KEY
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5AFA7A83
 apt-get update
 
 # Configure Kurento gnupg
@@ -54,36 +56,6 @@ fi
 
 # For backwards compatibility with kurento_clone_repo / Update to use github instead of gerrit
 export KURENTO_GIT_REPOSITORY=${KURENTO_GIT_REPOSITORY}
-
-# Configure private bower Repository
-cat >/root/.bowerrc << EOL
-{
-   "registry": "http://bower.kurento.org:5678" ,
-   "strict-ssl": false
-}
-EOL
-
-CODE_KURENTO_ORG=$(getent hosts code.kurento.org | awk '{ print $1 }')
-while [ -z $CODE_KURENTO_ORG ]
-do
-  CODE_KURENTO_ORG=$(getent hosts code.kurento.org | awk '{ print $1 }')
-done
-
-MAVEN_KURENTO_ORG=$(getent hosts maven.kurento.org | awk '{ print $1 }')
-while [ -z $MAVEN_KURENTO_ORG ]
-do
-  MAVEN_KURENTO_ORG=$(getent hosts maven.kurento.org | awk '{ print $1 }')
-done
-
-BOWER_KURENTO_ORG=$(getent hosts bower.kurento.org | awk '{ print $1 }')
-while [ -z $BOWER_KURENTO_ORG ]
-do
-  BOWER_KURENTO_ORG=$(getent hosts bower.kurento.org | awk '{ print $1 }')
-done
-
-echo "$CODE_KURENTO_ORG code.kurento.org" >> /etc/hosts
-echo "$MAVEN_KURENTO_ORG maven.kurento.org" >> /etc/hosts
-echo "$BOWER_KURENTO_ORG bower.kurento.org" >> /etc/hosts
 
 echo "[kurento_ci_container_entrypoint] Network configuration"
 ip addr list
