a900e996a730f59bb9e27ea4c2d6a0d19ce2c80a
Fede Diaz <nordri@gmail.com>
2018-04-05 12:47:07 +0200

    Changing path to opt to jenkins_home to be able to share with other containers

--- kurento_ci_container_job_setup.sh
+++ kurento_ci_container_job_setup.sh
@@ -133,7 +133,7 @@ docker run \
   -v /var/jenkins_home/test-files:$CONTAINER_TEST_FILES \
   -w $CONTAINER_TEST_FILES \
   kurento/svn-client:1.0.0 \
-  /opt/adm-scripts/kurento_update_test_files.sh || {
+  $CONTAINER_ADM_SCRIPTS/kurento_update_test_files.sh || {
     echo "[kurento_ci_container_job_setup] ERROR: Command failed: docker run kurento_update_test_files"
     exit $?
   }
@@ -271,7 +271,7 @@ docker run \
   -u "root" \
   -w "$CONTAINER_WORKSPACE" \
     $CONTAINER_IMAGE \
-      /opt/adm-scripts/kurento_ci_container_entrypoint.sh $BUILD_COMMAND
+      $CONTAINER_ADM_SCRIPTS/kurento_ci_container_entrypoint.sh $BUILD_COMMAND
 status=$?
 
 # Change worspace ownership to avoid permission errors caused by docker usage of root
